1a158fc5b3ce9dd95e8d9784f3ba6cd1af1ff389
Markus Beuckelmann <email@markus-beuckelmann.de>
2015-01-03 10:23:08 +0100

    Add Flask boilerplate

---
+++ Batyr/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('base.html')
+
+if __name__ == '__main__':
+	app.run()
