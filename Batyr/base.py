24aa0193968bfb7dd395ce309430551760476c44
Markus Beuckelmann <email@markus-beuckelmann.de>
2015-01-04 12:03:53 +0100

    Add a route to provide random Tatoeba items as JSON

--- Batyr/base.py
+++ Batyr/base.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask import render_template
+
+import json
+import os
+from random import randint
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return render_template('base.html')
+data = {}
+
+@app.before_first_request
+def load():
+
+	global data
+
+	path = 'data'
+	for f in os.listdir(path):
+		if f.endswith('.json'):
+			language = f.replace('.json', '')
+			data[language] = json.load(open('/'.join([path, f]), 'r'))
+
+@app.route('/items/random/<language>', methods = ['GET'])
+def items(language):
+
+	if data.has_key(language):
+		return jsonify(data[language][randint(0, len(data[language]))])
+	else:
+		abort(404)
 
 if __name__ == '__main__':
 	app.run()
