689bc8bb99511d6a9e2f75a65235485399c8b15d
Markus Beuckelmann <email@markus-beuckelmann.de>
2015-01-05 12:17:58 +0100

    Add content

--- Batyr/base.py
+++ Batyr/base.py
@@ -23,6 +23,10 @@ def load():
 			language = f.replace('.json', '')
 			data[language] = json.load(open('/'.join([path, f]), 'r'))
 
+@app.route('/<language>', methods = ['GET'])
+def languages(language):
+	return render_template('batyr.html', language = language)
+
 @app.route('/items/random/<language>', methods = ['GET'])
 def items(language):
 
@@ -32,4 +36,6 @@ def items(language):
 		abort(404)
 
 if __name__ == '__main__':
+
+	app.debug = True
 	app.run()
